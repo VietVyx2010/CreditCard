2d4b367620489dc4b2aa1086a75f709f075d77ad
VietVyx2010 <83071749+VietVyx2010@users.noreply.github.com>
2021-09-01 19:41:53 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    const themeBtn = document.getElementById('toggleBtn');
+    themeBtn.addEventListener('click', function () {
+      // Lấy thuộc tính data-theme
+      const root = document.querySelector(':root');
+      const isLightMode =
+        root.getAttribute('data-theme') === 'dark' ? false : true;
+      // toggle theme mode
+      if (isLightMode) {
+        document.querySelector("body").style.backgroundImage = "url('/img/BACKGROUND-DARK.png')"
+        root.setAttribute('data-theme', 'dark');
+      } else {
+        document.querySelector("body").style.backgroundImage = "url('/img/BACKGROUND.png')"
+        root.setAttribute('data-theme', 'light');
+      }
+      // thay đổi vị trí của button
+      this.classList.toggle('active');
+    });
+  };
